046fc9a824bd0320d2841357ec61494d40fad345
kosen <kosen@localhost.localdomain>
2015-09-25 12:27:02 +0900

    update

--- tools/usart/recive_usart.py
+++ tools/usart/recive_usart.py
@@ -11,11 +11,14 @@ def main():
     pic = serial.Serial('/dev/ttyUSB0', 9600)
     while True:
         cmd = ord(pic.read())
-        if (cmd > 128):
+        sig = '+'
+        if (cmd >= 128):
             cmd_10 = cmd - 128
+            sig = '-'
         else:
             cmd_10 = cmd
-        print("{0}\t\tspeed: {1}\t\t0b{2}".format(hex(cmd), cmd_10,  bin(cmd)[2:].zfill(8)))
+            sig = '+'
+        print("{0}\t\tspeed: {1}{2}\t\t0b{3}".format(hex(cmd), sig, cmd_10,  bin(cmd)[2:].zfill(8)))
 
 
 if __name__ == '__main__':
